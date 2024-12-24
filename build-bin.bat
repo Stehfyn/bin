3ed3c498f275f96f991418188ceadb12f0767799
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2024-12-23 22:48:21 -0800

    "Ohhh yeahhh" (koolaid man)

--- build-bin.bat
+++ build-bin.bat
@@ -8,5 +8,10 @@ call nl\build.bat
 echo %cd%
 copy /v /b "%CD%\nl\x64\nl.exe" "%CD%\nl.exe"
 
+::ntstr
+call ntstr\build.bat
+echo %cd%
+copy /v /b "%CD%\ntstr\out\x64\ntstrx64.exe" "%CD%\ntstrx64.exe"
+
 popd
 endlocal
--- download-external.bat
+++ download-external.bat
@@ -19,10 +19,18 @@ msiexec /a external\cache\7z2201-x64.msi /qn TARGETDIR="%CD%\external\7z2201-x64
 if not exist external\cache\uncrustify-0.80.1_f-win64.zip (
 curl -L --show-error https://github.com/uncrustify/uncrustify/releases/download/uncrustify-0.80.1/uncrustify-0.80.1_f-win64.zip -o external\cache\uncrustify-0.80.1_f-win64.zip
 )
-::install Uncrustify
+::unzip Uncrustify
 if not exist external\uncrustify-0.80.1_f-win64\bin\uncrustify.exe (
 external\7z2201-x64\Files\7-Zip\7z.exe x -y external\cache\uncrustify-0.80.1_f-win64.zip -oexternal\uncrustify-0.80.1_f-win64\
 copy /y /v external\uncrustify-0.80.1_f-win64\bin\uncrustify.exe uncrustify.exe
+)
+::download SystemInformer
+if not exist external\cache\systeminformer-3.1.24298-release-bin.zip (
+curl -L --show-error https://sourceforge.net/projects/systeminformer/files/systeminformer-3.1.24298-release-bin.zip/download -o external\cache\systeminformer-3.1.24298-release-bin.zip
+)
+::unzip SystemInformer
+if not exist external\systeminformer-3.1.24298-release-bin\amd64\SystemInformer.exe (
+external\7z2201-x64\Files\7-Zip\7z.exe x -y external\cache\systeminformer-3.1.24298-release-bin.zip -oexternal\systeminformer-3.1.24298-release-bin\
 )
 popd
 endlocal
